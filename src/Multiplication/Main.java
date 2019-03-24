46cf9743b3d72cb48c0e6b662c7b7046870c8967
Gorzos <gorzos25@gmail.com>
2019-03-24 21:05:11 +0200

    tree

--- src/Multiplication/Main.java
+++ src/Multiplication/Main.java
@@ -2,11 +2,18 @@ package Multiplication;
 
 public class Main {
     public static void main(String [] args) {
-        int mult2 = 5;
-        int mult3 = 6;
+        int mult1 = 1;
+        int mult2 = 2;
+        int mult3 = 3;
+        int mult4 = 4;
+        int mult5 = 5;
+        int mult6 = 6;
+        int mult7 = 7;
+        int mult8 = 8;
+        int mult9 = 9;
 
-        for(int i = 1; i <= 10; i++){
-            System.out.println((mult2 + "*" + i + "=" + mult2 * i + " ") + (mult3 + "*" + i + "=" + mult3 * i + " "));
+        for(int i = 1; i <= 9; i++){
+            System.out.println((mult1 + "*" + i + "=" + mult1 * i + " ") + (mult2 + "*" + i + "=" + mult2 * i + " ") + (mult3 + "*" + i + "=" + mult3 * i + " ") + (mult4 + "*" + i + "=" + mult4 * i + " ") + (mult5 + "*" + i + "=" + mult5 * i + " ") + (mult6 + "*" + i + "=" + mult6 * i + " ") + (mult7 + "*" + i + "=" + mult7 * i + " ") + (mult8 + "*" + i + "=" + mult8 * i + " ") + (mult9 + "*" + i + "=" + mult9 * i + " "));
         }
     }
 }
---
+++ src/Tree/Main.java
@@ -0,0 +1,10 @@
+package Tree;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("\n* \n** \n***  \n**** \n*****");
+        System.out.println("\n\n***** \n**** \n*** \n** \n*");
+
+    }
+
+}
